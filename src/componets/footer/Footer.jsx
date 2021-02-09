940487de8ee1ca97fbc6067e1a5f7bc4e96176ce
Christian <christianrenovel83@gmail.com>
2021-02-09 21:52:03 +0000

    footer

---
+++ src/componets/footer/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <div>
+            <footer className="bg-light text-center text-lg-start">
+                <div className="text-center p-3 bg-darck">
+                    © 2021 Copyright:
+                <a className="text-dark" href="https://christianrenovell.com/">christianrenovell.com</a>
+                </div>
+            </footer>
+        </div>
+    );
+}
+export default Footer;
